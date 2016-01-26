f5000165185e6ad6c0e7a5cdcacf59bb4cf99ad9
David Haro <david.haro@me.com>
2016-01-25 16:02:16 -0800

    dHash implemmentation

--- imagehashing/Examples/dhash_ex.py
+++ imagehashing/Examples/dhash_ex.py
@@ -1,27 +1,27 @@
 from PIL import Image
 import imagehash
 
-print ("Use pHash to determine if images are similar/same")
+print ("Use dHash to determine if images are similar/same")
 
 # Generate hashes for all
 print ("screenshot #1")
-hash = imagehash.phash(Image.open('test.png'))
+hash = imagehash.dhash(Image.open('test.png'))
 print (hash)
 
 print ("\nscreenshot #2")
-otherhash = imagehash.phash(Image.open('other.png'))
+otherhash = imagehash.dhash(Image.open('other.png'))
 print (otherhash)
 
 print ("\nRandom screenshot")
-unrelated_image = imagehash.phash(Image.open('wrong.png'))
+unrelated_image = imagehash.dhash(Image.open('wrong.png'))
 print (unrelated_image)
 
 print ("\nBlank white FFFFFF.png")
-white = imagehash.phash(Image.open('FFFFFF.png'))
+white = imagehash.dhash(Image.open('FFFFFF.png'))
 print (white)
 
 print ("\nBlank black 000000.png")
-black = imagehash.phash(Image.open('000000.png'))
+black = imagehash.dhash(Image.open('000000.png'))
 print (black)
 
 print ("\nCompare first two screenshots")
